c3e0a648a5c4ba4afc1dd489d200d86edbe2e087
Joseph Gordon <115722035+GoodNoodle23@users.noreply.github.com>
2025-02-16 02:06:02 -0500

    changed files for an assignment

--- assignment.py
+++ assignment.py
@@ -13,3 +13,30 @@
 #The month is February and the current season is Winter.
 #Remember to also complete the flowchart. It is strongly advised that you do the flowchart first,
 #as this will help you write the code.
+
+month = int(input("What month is it? (1-12): "))
+
+if month == int(1):
+    print("The month is Janurary and the current season is Winter")
+elif month == int(2):
+    print(" The month is Feburary and the current season is Winter")
+elif month == int(12):
+    print(" The month is December and the current season is Winter")
+elif month == int(3):
+    print("The month is March and the current season is Spring")
+elif month == int(4):
+    print("The month is April and the current season is Spring")
+elif month == int(5):
+    print("The month is May and the current season is Spring")
+elif month == int(6):
+    print("The month is June and the current season is Summer")
+elif month == int(7):
+    print("The month is July and the current season is Summer")
+elif month == int(8):
+    print("The month is August and the current season is Summer")
+elif month == int(9):
+    print("The month is September and the current season is Fall")
+elif month == int(10):
+    print("The month is October and the current season is Fall")
+elif month == int(11):
+    print("The month is November and the current season is Fall")
